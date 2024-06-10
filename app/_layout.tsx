3e076b7fe874883b6868fcdb0d4b6b1403b383d1
Pim van Milt <pim.van.milt@dpdk.com>
2024-06-10 22:58:35 +0200

    trash but need this on laptop

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+import { Slot } from "expo-router";
+import { ContentfulClientContext } from "@/ctx/ContentfulClientContext";
+import { useEffect, useState } from "react";
+export default function Root() {
+  // Set up the auth context and render our layout inside of it.
+  const [client, setClient] = useState();
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    try {
+      const contentful = require("contentful");
+      const contentfulClient = contentful.createClient({
+        accessToken: process.env.EXPO_PUBLIC_ACCESS_TOKEN,
+        space: process.env.EXPO_PUBLIC_SPACE,
+      });
+      setClient(contentfulClient);
+    } catch (error) {
+      console.log(error);
     }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+  }, []);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <ContentfulClientContext.Provider value={{ client, setClient }}>
+      <Slot />
+    </ContentfulClientContext.Provider>
   );
 }
