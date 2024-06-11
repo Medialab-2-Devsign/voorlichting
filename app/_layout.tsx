c9db8577170f743d67b3ced6be2f4d7762b61acf
Pim van Milt <pim.milt@gmail.com>
2024-06-11 13:39:26 +0200

    client cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,9 @@
 import { Slot } from "expo-router";
-import { ContentfulClientContext } from "@/ctx/ContentfulClientContext";
-import { useEffect, useState } from "react";
+import { ContentfulClientProvider } from "@/ctx/ContentfulClientContext";
 export default function Root() {
-  // Set up the auth context and render our layout inside of it.
-  const [client, setClient] = useState();
-
-  useEffect(() => {
-    try {
-      const contentful = require("contentful");
-      const contentfulClient = contentful.createClient({
-        accessToken: process.env.EXPO_PUBLIC_ACCESS_TOKEN,
-        space: process.env.EXPO_PUBLIC_SPACE,
-      });
-      setClient(contentfulClient);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
   return (
-    <ContentfulClientContext.Provider value={{ client, setClient }}>
+    <ContentfulClientProvider>
       <Slot />
-    </ContentfulClientContext.Provider>
+    </ContentfulClientProvider>
   );
 }
--- ctx/ContentfulClientContext.jsx
+++ ctx/ContentfulClientContext.jsx
@@ -1,3 +1,11 @@
 import { createContext } from "react";
 
 export const ContentfulClientContext = createContext();
+
+export const ContentfulClientProvider = ({ children }) => {
+  return (
+    <ContentfulClientContext.Provider value={{}}>
+      {children}
+    </ContentfulClientContext.Provider>
+  );
+};
