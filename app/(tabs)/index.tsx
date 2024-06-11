58c8552b7652f0255b5a0833ef00890830d71fad
Mark Hobbel <mark.hobbel@live.nl>
2024-06-11 14:07:16 +0200

    fetch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,20 +4,57 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-     <Text> HAllo</Text>
-    </ParallaxScrollView>
-  );
+  const [data, setData] = useState(null);
+
+  const fetchTheData = async () => {
+    try {
+    const apiUrl = 'https://cdn.contentful.com/spaces/yshlrg4y56c9/environments/master/entries?access_token=JIUK52a12pwwal6mqdHN4FWIssE7nrtcR0bs7Xsogpk';
+    
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    setData(data.items);
+    console.log(data.items[1])
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+};
+
+  useEffect(() => {
+    fetchTheData();
+  }, []);
+
+  if (data) {
+    return (
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        headerImage={
+          <Image
+            source={require('@/assets/images/partial-react-logo.png')}
+            style={styles.reactLogo}
+          />
+        }>
+       <Text> HAllo</Text>
+      </ParallaxScrollView>
+    );
+  } else {
+    return (
+      <ParallaxScrollView>
+        <Text>
+          loading...
+        </Text>
+      </ParallaxScrollView>
+    )
+  }
+ 
 }
 
 const styles = StyleSheet.create({
