0f56ad3523e47679bbc290cd6c59b97a7b76eee1
Mark Hobbel <mark.hobbel@live.nl>
2024-06-14 12:26:19 +0200

    onboarding progression

--- components/onboarding/OnboardingScreen.js
+++ components/onboarding/OnboardingScreen.js
@@ -1,13 +1,40 @@
-import React, { useEffect } from 'react';
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
-import colors from '../../frontend/Colors'
-import GlobeImage from '../../assets/images/Globe.png'
-
+import colors from '../../frontend/Colors';
+import GlobeImage from '../../assets/images/Globe.png';
 
 const OnboardingScreen = (props) => {
+  const [selectedLanguage, setSelectedLanguage] = useState(null);
+
+  const languages = [
+    { label: 'Nederlands', value: 'nl' },
+    { label: 'English', value: 'en' },
+    { label: 'Español', value: 'es' },
+    { label: 'Français', value: 'fr' },
+    { label: 'Deutsch', value: 'de' },
+
+  ];
+
+  const onLanguageSelect = (language) => {
+    setSelectedLanguage(language);
+  };
+
+  const renderLanguageList = () => {
+    return languages.map((language, index) => (
+      <TouchableOpacity
+        key={index}
+        style={[styles.languageItem, selectedLanguage === language.value && styles.selectedItem]}
+        onPress={() => onLanguageSelect(language.value)}
+      >
+        <Text style={[styles.languageText, selectedLanguage === language.value && styles.selectedText]}>{language.label}</Text>
+      </TouchableOpacity>
+    ));
+  };
+
   const onDone = () => {
-    props.navigation.navigate('HomeTab'); // Navigeer naar het Home component
+    // Hier kun je de geselecteerde taal verder verwerken
+    props.navigation.navigate('HomeTab');
   };
 
   return (
@@ -21,14 +48,61 @@ const OnboardingScreen = (props) => {
         },
         {
           backgroundColor: colors.grey__100,
-          image: <Image source={GlobeImage} />,
-          title: 'Tweede scherm',
-          subtitle: 'Meer informatie over je app.',
+          image: <Image source={GlobeImage} style={styles.image} />,
+          subtitle: (
+            <View style={styles.languageList}>
+              <ScrollView contentContainerStyle={styles.scrollView}>
+                {renderLanguageList()}
+              </ScrollView>
+            </View>
+          ),
         },
-        
       ]}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  languageList: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '50%',
+    marginBottom: 50,
+  },
+  scrollView: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  languageItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: colors.blue__700,
+    width: '100%',
+    alignItems: 'center',
+  },
+  selectedItem: {
+    backgroundColor: colors.blue__700,
+  },
+  languageText: {
+    fontSize: 16,
+    color: colors.blue__700,
+  },
+  selectedText: {
+    color: '#ffffff', // Witte tekstkleur voor geselecteerde taal
+  },
+});
 
 export default OnboardingScreen;
