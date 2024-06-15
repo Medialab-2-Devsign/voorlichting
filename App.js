9f50b8d34e2e5853a8f60253bdfe318d2422d3c3
Pim van Milt <pim.van.milt@dpdk.com>
2024-06-15 15:11:58 +0200

    import cleanup

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 // App.js
 
-import React, { useEffect } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { I18n } from "i18n-js";
 
 import OnboardingScreen from "./components/onboarding/OnboardingScreen";
 import HomeScreen from "./components/HomeScreen";
