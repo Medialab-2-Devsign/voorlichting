6110578a39949dda3608edcb9bceb698390e5e14
Pim van Milt <pim.van.milt@dpdk.com>
2024-06-17 20:23:20 +0200

    translation updates

--- components/onboarding/OnboardingScreen.js
+++ components/onboarding/OnboardingScreen.js
@@ -1,9 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { View, ScrollView, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
-import Onboarding from 'react-native-onboarding-swiper';
-import colors from '../../frontend/Colors';
-import GlobeImage from '../../assets/images/Globe.png';
-import { TranslationContext } from '../../contexts/TranslationContext'; // Importeer TranslationContext
+import React, { useContext, useEffect, useState } from "react";
+import {
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Image,
+} from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
+import colors from "../../frontend/Colors";
+import GlobeImage from "../../assets/images/Globe.png";
+import { TranslationContext } from "../../contexts/TranslationContext"; // Importeer TranslationContext
 
 const OnboardingScreen = (props) => {
   const [selectedLanguage, setSelectedLanguage] = useState(null);
@@ -18,26 +25,40 @@ const OnboardingScreen = (props) => {
     return i18n.availableLocales.map((language, index) => (
       <TouchableOpacity
         key={index}
-        style={[styles.languageItem, selectedLanguage === language.code && styles.selectedItem]}
+        style={[
+          styles.languageItem,
+          selectedLanguage === language.code && styles.selectedItem,
+        ]}
         onPress={() => onLanguageSelect(language.code)}
       >
-        <Text style={[styles.languageText, selectedLanguage === language.code && styles.selectedText]}>{language.languageString}</Text>
+        <Text
+          style={[
+            styles.languageText,
+            selectedLanguage === language.code && styles.selectedText,
+          ]}
+        >
+          {language.languageString}
+        </Text>
       </TouchableOpacity>
     ));
   };
 
   const onDone = () => {
-    props.navigation.navigate('HomeTab');
+    props.navigation.navigate("HomeTab");
   };
 
   return (
     <Onboarding
       onDone={onDone}
+      onSkip={onDone}
+      skipLabel={i18n.t("ui.skipButton")}
+      nextLabel={i18n.t("ui.nextButton")}
       pages={[
         {
           backgroundColor: colors.blue__700,
-          title: i18n.t('hello'), // Gebruik i18n.t om vertalingen te verkrijgen
-          // subtitle: i18n.t('welcomeSubtitle'), // Gebruik i18n.t om vertalingen te verkrijgen
+          title: i18n.t("onboard.title"), // Gebruik i18n.t om vertalingen te verkrijgen
+          subtitle: i18n.t("onboard.subtitle"), // Gebruik i18n.t om vertalingen te verkrijgen
+          image: <Image source={GlobeImage} style={styles.image} />,
         },
         {
           backgroundColor: colors.grey__100,
@@ -59,32 +80,35 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   languageList: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '50%',
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "50%",
     marginBottom: 50,
   },
   scrollView: {
     flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "auto",
+    paddingHorizontal: 50,
+    paddingBottom: 100,
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 10,
   },
   languageItem: {
     paddingVertical: 10,
     paddingHorizontal: 20,
     marginBottom: 10,
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     borderRadius: 10,
     borderWidth: 1,
     borderColor: colors.blue__700,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   selectedItem: {
     backgroundColor: colors.blue__700,
@@ -94,7 +118,7 @@ const styles = StyleSheet.create({
     color: colors.blue__700,
   },
   selectedText: {
-    color: '#ffffff', 
+    color: "#ffffff",
   },
 });
 
